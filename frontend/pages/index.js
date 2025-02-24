8e265311fc62fcdac9f10a352af9ffdd2b9cc86d
Miyu Masayama <miyumasayama@Miyus-Laptop.local>
2025-02-24 12:17:06 +0900

    add:inputランを追加

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,26 @@
 
-import { Alert, Button } from 'reactstrap'
+import { Alert, Button, Col, InputGroup, InputGroupText, Row, Input } from 'reactstrap'
 const index = () => {
   return (
-    <div>
-      <div>
-        <Alert color="primary" >
-          Hello
-        </Alert>
-        <Button color="primary">
-          Hello from nextjs
-        </Button>
-      </div>
+    <div className='container-fluid'>
+      <Row>
+        <Col>
+          <div className='search'>
+            <InputGroup>
+              <InputGroupText>search</InputGroupText>
+              <Input placeholder="レストラン名を入力" />
+            </InputGroup>
+          </div>
+        </Col>
+      </Row>
+      <style jsx>{` 
+        .search {
+          margin: 20px;
+          width: 500px;
+        }
+      `}
+      </style>
+
     </div>
   );
 }
