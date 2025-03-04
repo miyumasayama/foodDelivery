dca97d86eed78a16d417ba79808653ce057aee53
Miyu Masayama <miyumasayama@Miyus-Laptop.local>
2025-03-04 17:56:40 +0900

    ユーザー登録用のデータ受け渡し

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,12 +3,21 @@ import App from "next/app"
 import Head from "next/head"
 import Layout from "../components/layout"
 import withData from "../lib/apollo"
+import AppContext from '../context/context'
 
 class MyApp extends App {
+
+  // classコンポーネントのためuseStateを使えないが、意味的にはconst [user, setUser] = useState(null)と同じ
+  state = {
+    user: null
+  }
+  setUser = (user) => {
+    this.setState({ user })
+  }
   render() {
     const { Component, pageProps } = this.props
     return (
-      <>
+      <AppContext.Provider value={{ user: this.state.user, setUser: this.setUser }}>
         <Head>
           <link
             rel="stylesheet"
@@ -19,7 +28,7 @@ class MyApp extends App {
           {/* Componentはすべてのコンポーネントを指す */}
           <Component {...pageProps} />
         </Layout>
-      </>
+      </AppContext.Provider>
     )
   }
 }
--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,6 +1,13 @@
 import { Button, Col, Container, Form, FormGroup, Input, Label, Row } from "reactstrap"
+import { register as registerUser } from "../lib/auth"
+import { useState } from "react"
 
 const register = () => {
+  const [data, setData] = useState({ username: '', email: '', password: '' })
+  const handleClick = () => {
+    registerUser(...data)
+  }
+
   return (
     <Container>
       <Row>
@@ -15,22 +22,22 @@ const register = () => {
               <fieldset>
                 <FormGroup>
                   <Label>ユーザー名</Label>
-                  <Input type="text" name="username" syle={{ height: 50, fontSize: "1.2rem" }} />
+                  <Input type="text" name="username" onChange={(e) => setData({ ...data, username: e.target.value })} syle={{ height: 50, fontSize: "1.2rem" }} />
                 </FormGroup>
                 <FormGroup>
                   <Label>メールアドレス</Label>
-                  <Input type="text" name="email" syle={{ height: 50, fontSize: "1.2rem" }} />
+                  <Input type="text" name="email" onChange={(e) => setData({ ...data, email: e.target.value })} syle={{ height: 50, fontSize: "1.2rem" }} />
                 </FormGroup>
                 <FormGroup>
                   <Label>パスワード</Label>
-                  <Input type="password" name="password" syle={{ height: 50, fontSize: "1.2rem" }} />
+                  <Input type="password" name="password" onChange={(e) => setData({ ...data, password: e.target.value })} syle={{ height: 50, fontSize: "1.2rem" }} />
                 </FormGroup>
                 <span>
                   <a href="">
                     <small>パスワードをお忘れですか?</small>
                   </a>
                 </span>
-                <Button style={{ float: 'right', width: 120 }} color="primary">登録</Button>
+                <Button style={{ float: 'right', width: 120 }} color="primary" onClick={() => handleClick()}>登録</Button>
               </fieldset>
             </Form>
           </section>
