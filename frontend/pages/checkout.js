3c787b80b6e1fa69cdd6886c104b557add46e1df
Miyu Masayama <miyumasayama@Miyus-Laptop.local>
2025-03-17 19:25:54 +0900

    カード情報入力のコンポーネント作成

--- frontend/pages/checkout.js
+++ frontend/pages/checkout.js
@@ -1,8 +1,13 @@
 import { Col, Row } from "reactstrap";
 import Cart from "../components/cart";
 import { CheckoutForm } from "../components/checkoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 const checkout = () => {
+  const stripePromise = loadStripe(
+    "pk_test_51R2lHtKxzJxAC3pa3NihL637edyHS49Btu5b8mDfrkfJp5IYnLo8pyrVSggPGFGBn5QWnambEyRlRfGFbhLwVrv600KQ8875QQ"
+  )
   return (
     <Row>
       <Col style={{ padingRight: 0 }} sm={{ size: 3, order: 1, offset: 2 }}>
@@ -10,7 +15,9 @@ const checkout = () => {
         <Cart />
       </Col>
       <Col style={{ padingLeft: 5 }} sm={{ size: 6, order: 2 }}>
-        <CheckoutForm />
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
       </Col>
     </Row>
   );
